1001222bd7aede90796052f1931686b1139a955b
Lorenza-1 <lorenzarichard1@gmail.com>
2026-08-29 20:37:25 +0000

    Initial commit

---
+++ src/main/java/com/mycompany/hospitalpatientsystem/HospitalPatientSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospitalpatientsystem;
+
+/**
+ *
+ * @author emeris
+ */
+public class HospitalPatientSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
